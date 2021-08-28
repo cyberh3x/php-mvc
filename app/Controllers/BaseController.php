0d33e4a1e8f4176d08796bc191157aed47bb87b3
sajjadN7798 <sajjad.n18@outlook.com>
2021-08-28 16:37:51 +0430

    Initial Project

--- app/Controllers/BaseController.php
+++ app/Controllers/BaseController.php
@@ -7,12 +7,12 @@ use Jenssegers\Blade\Blade;
 class BaseController
 {
     protected string $views = APP_ROOT . '/views/';
-    protected string $cachePath = APP_ROOT . '/bootstrap/cache/';
+    protected string $viewsCachePath = APP_ROOT . '/storage/framework/views';
 
     protected function render(string $viewPath, array $data = [])
     {
         try {
-            $blade = new Blade($this->views, $this->cachePath);
+            $blade = new Blade($this->views, $this->viewsCachePath);
             echo $blade->render($viewPath, $data);
             return true;
         } catch (\Exception $exception) {
--- public/index.php
+++ public/index.php
@@ -1,8 +1,5 @@
 <?php
 require_once '../vendor/autoload.php';
 require_once '../routes/web.php';
-require_once '../index.php';
+require_once '../bootstrap/index.php';
 require_once '../app/Router.php';
-//require_once '../index.php';
-
-
